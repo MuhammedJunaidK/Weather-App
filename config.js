511641588d9c8af637ce79fcbfedd44827f6850d
Muhammed Junaid K <63506985+MuhammedJunaidK@users.noreply.github.com>
2023-04-17 18:23:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "96a4310d06ebcf47cff96e67868180fe"
+    }
+}
+
+module.exports = constants;
